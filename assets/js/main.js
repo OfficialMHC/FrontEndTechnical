bd754af257de7569bb880d003dc5daa6d19ba43e
OfficialMHC <official.dev.mhc@gmail.com>
2021-12-12 23:14:04 +0600

    Product Return

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,6 +17,7 @@ let retrievedData = localStorage.getItem('frontEndTechnical');
 /**----- PRODUCTS SHOW INTO TABLE -----**/
 products(JSON.parse(retrievedData))
 document.querySelector('#btnBook').setAttribute('data-products', retrievedData);
+document.querySelector('#btnReturn').setAttribute('data-products', retrievedData);
 
 
 
@@ -93,29 +94,38 @@ searchBox.addEventListener('keyup', searchProduct);
 
 
 /**----- SHOWING PRODUCTS INTO PRODUCT BOOK MODAL -----**/
-$('#productBook').on('show.bs.modal', function (event) {
+$('#productBookOrReturn').on('show.bs.modal', function (event) {
     let button = $(event.relatedTarget)
     let getProducts = button.data('products');
+    let getDataType = button.data('type');
+
+    $('#dataType').val(getDataType);
+
+    if (getDataType == 'Return') {
+        $('#productBookOrReturnLabel').html('<i class="bi bi-arrow-return-right"></i>  Return a Product')
+    }
 
     let products = document.getElementById('products');
 
     for (var i = 0; i < getProducts.length; i++) {
-        products.innerHTML = products.innerHTML + '<option value="' + getProducts[i].name + '" data-price="'+ getProducts[i].price +'" data-rental-period="'+ getProducts[i].minimum_rent_period +'">' + getProducts[i].name + '</option>';
+        products.innerHTML = products.innerHTML + '<option value="' + getProducts[i].name + '" data-price="'+ getProducts[i].price +'" data-mileage="'+ getProducts[i].mileage +'" data-rental-period="'+ getProducts[i].minimum_rent_period +'">' + getProducts[i].name + ' / ' + getProducts[i].code + '</option>'
     }
 })
 
 
-function getPrice(obj)
+function getProductInfo(obj)
 {
     let productName = obj.options[obj.selectedIndex].value;
     let productPrice = obj.options[obj.selectedIndex].getAttribute('data-price');
+    let productMileage = obj.options[obj.selectedIndex].getAttribute('data-mileage');
 
     document.getElementById('selectedProductName').value=productName;
     document.getElementById('selectedProductPrice').value=productPrice;
+    document.getElementById('selectedProductMileage').value='Mileage: '+productMileage
 }
 
 
-function productBook()
+function productBookOrReturn()
 {
     let fromDate = new Date(document.getElementById('fromDate').value);
     let toDate = new Date(document.getElementById('toDate').value);
@@ -133,35 +143,45 @@ function productBook()
 
     let productName = document.getElementById('selectedProductName').value;
 
+    let getDataType = document.getElementById('dataType').value;
+    let type = getDataType;
+    if (getDataType == 'Return') {
+        type = getDataType;
+    }
 
-    Swal.fire({
-        title: 'Your Estimated Price is $' + estimatedPrice +'.',
-        text: "Do you want to procedure?",
-        icon: 'warning',
-        showCancelButton: true,
-        confirmButtonColor: '#3085d6',
-        cancelButtonColor: '#d33',
-        confirmButtonText: 'Yes, Procedure!'
-      }).then((result) => {
-        if (result.isConfirmed) {
-          Swal.fire(
-            'Success!',
-            'Your Product has been Booked Successfully.',
-            'success'
-          )
+    if (productName == '') {
+        alert('Please Select a Product')
+    }else if (fromDate == 'Invalid Date') {
+        alert('Please Select From Date')
+    }else if (toDate == 'Invalid Date') {
+        alert('Please Select To Date')
+    }else if (fromDate > toDate) {
+        alert('You can\'t select To Date before From Date')
+    } else {
+        Swal.fire({
+            title: 'Your Estimated Price is $' + estimatedPrice +'.',
+            text: "Do you want to procedure?",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, Procedure!'
+          }).then((result) => {
+            if (result.isConfirmed) {
+              Swal.fire(
+                'Success!',
+                'Your Product has been ' + type + ' Successfully.',
+                'success'
+              )
+    
+              createProductBookOrReturnJSON(productName, estimatedPrice, type)
+    
+            }
+        })
 
-          createProductBookJSON(productName, estimatedPrice)
-
-        }
-      })
-
-      document.getElementById('fromDate').value=''
-      document.getElementById('toDate').value=''
-      document.getElementById('selectedProductName').value=''
-      document.getElementById('selectedProductPrice').value=''
-      document.getElementById('estimatedPrice').value=''
-      document.getElementById('products').value=''
-      $('.select2').select2();
+        document.getElementById('btnProductBookOrReturn').setAttribute('data-dismiss', 'modal')
+        dismissModal()
+    }
 }
 
 
@@ -180,16 +200,35 @@ function confirmProductBook()
 
 
 
-function createProductBookJSON(name, price) {
-    productBookJSONObj = [];
+function createProductBookOrReturnJSON(name, price, type) {
+    productBookOrReturnJSONObj = [];
     
     item = {}
     item ["productName"] = name;
     item ["estimatedPrice"] = price;
 
-    productBookJSONObj.push(item);
+    productBookOrReturnJSONObj.push(item);
 
-    console.log(productBookJSONObj);
+    console.log(productBookOrReturnJSONObj);
 
-    localStorage.setItem('productBookData', JSON.stringify(productBookJSONObj)); 
+    if(type == 'Book') {
+        localStorage.setItem('productBookData', JSON.stringify(productBookOrReturnJSONObj)); 
+    } else {
+        localStorage.setItem('productReturnData', JSON.stringify(productBookOrReturnJSONObj)); 
+    }
+    
+}
+
+
+function dismissModal()
+{
+    document.getElementById('productBookOrReturnLabel').innerHTML='<i class="bi bi-bookmark-check"></i>  Book a Product'
+    document.getElementById('fromDate').value=''
+    document.getElementById('toDate').value=''
+    document.getElementById('selectedProductName').value=''
+    document.getElementById('selectedProductPrice').value=''
+    document.getElementById('estimatedPrice').value=''
+    document.getElementById('products').value=''
+    document.getElementById('selectedProductMileage').value='Mileage:'
+    $('.select2').select2();
 }
